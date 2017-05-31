9479add3e7daf62bf701d6332eb91eb249ce6364
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-05-30 20:56:38 -0400

    updates and fixes conflicts

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,7 @@
-searchButton
+//The backend that ultimately powers the interface of the movie search. The following API's will be used:
+//OMDB, youtube, Giphy
+//
+
+$(".search-form").submit(function(event) {
+    alert("You submitted a movie");    
+})
