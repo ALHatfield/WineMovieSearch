c81dd5896474778ed27170aaa3248f247c0888b1
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-06-01 18:57:38 -0400

    removes extraneous files

--- assets/javascript/logic.js
+++
@@ -1,7 +0,0 @@
-//The backend that ultimately powers the interface of the movie search. The following API's will be used:
-//OMDB, youtube, Giphy
-//
-
-$(".search-form").submit(function(event) {
-    alert("You submitted a movie");    
-})
